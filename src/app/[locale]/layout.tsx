713d43e710304cf2881959fa4a80652370343b1c
Guoqi Sun <sun412926@gmail.com>
2024-11-22 05:55:00 +0800

    feat: update next-intl configuration

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,8 +5,10 @@ import { type Metadata } from "next"
 import { SessionProvider } from "next-auth/react"
 import { NextIntlClientProvider } from "next-intl"
 import { getMessages } from "next-intl/server"
+import { notFound } from "next/navigation"
 import { Toaster } from "react-hot-toast"
 import { ThemeProvider } from "~/components/theme-provider"
+import { routing } from "~/i18n/routing"
 import { auth } from "~/server/auth"
 import "~/styles/globals.css"
 import { TRPCReactProvider } from "~/trpc/react"
@@ -23,12 +25,28 @@ export default async function LocaleLayout({
 }: Readonly<{ children: React.ReactNode; params: { locale: string } }>) {
   const session = await auth()
   const messages = await getMessages()
+
+  // Ensure that the incoming `locale` is valid
+  // eslint-disable-next-line
+  if (!routing.locales.includes(locale as any)) {
+    notFound()
+  }
+
   return (
     <html
       lang={locale}
       className={`${GeistSans.variable}`}
       suppressHydrationWarning
     >
+      <head>
+        <script
+          defer
+          src="https://umami.guoqi.dev/script.js"
+          data-website-id="e3df813a-bc42-4da9-af6d-664b0b56250d"
+          data-domains="camlife.app"
+        />
+      </head>
+
       <body>
         <SessionProvider session={session}>
           <NextIntlClientProvider messages={messages}>
